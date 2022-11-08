604659d30df6545c3cabcbb4e622c505eaa7c44f
unknown <mosharafctg111@gmail.com>
2022-11-08 18:03:24 +0600

    added the services in get mongodb data

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors')
+require('dotenv').config();
 const port = process.env.PORT || 5000
 
 app.use(cors())
@@ -9,6 +11,31 @@ app.use(express.json())
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.uujg3og.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+    try{
+        const servicesCollection =client.db('touristguide').collection('services')
+        app.get('/services', async(req,res)=>{
+            const query = {}
+            const cursor = servicesCollection.find(query)
+            const services = await cursor.toArray();
+            res.send(services);
+        })
+
+    }
+    finally{
+
+    }
+}
+run().catch(error=> console.error(error))
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
